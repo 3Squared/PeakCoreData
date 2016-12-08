30435a92a6b2505757afe63f3ff75648cd45acab
Sam Oakley <sam.oakley@3squared.com>
2016-12-08 16:05:09 +0000

    Replace concurrent operation with THROperation pod

--- THRCoreData/CoreDataOperation.swift
+++ THRCoreData/CoreDataOperation.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import CoreData
+import THROperations
+import THRResult
 
 // Slightly experimental version of our core data operation that uses child context.
 // This means changes are saved up the chain rather than being merged in to the main context.
 
-open class CoreDataOperation: ConcurrentOperation {
+open class CoreDataOperation: ConcurrentOperation<Bool> {
     
     fileprivate let targetContext: NSManagedObjectContext
     fileprivate var childContext: NSManagedObjectContext!
@@ -24,7 +26,7 @@ open class CoreDataOperation: ConcurrentOperation {
     
     // MARK: - ConcurrentOperation Overrides
 
-    open override func execute() {
+    open override func run() {
         childContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
         childContext.parent = targetContext
         childContext.performAndWait {
@@ -44,18 +46,26 @@ extension CoreDataOperation {
     
     public func completeAndSave() {
         guard !isCancelled, childContext.hasChanges else {
-            return finish(withError: nil)
+            operationResult = Result { false }
+            return finish()
         }
         
         do {
             try childContext.save()
             self.save(parentContext: childContext.parent, completion: { [weak self] error in
                 guard let strongSelf = self else { return }
-                strongSelf.finish(withError: error)
+                strongSelf.operationResult = Result {
+                    if let contextError = error {
+                        throw contextError
+                    } else {
+                        return true
+                    }
+                }
             })
         } catch {
             print("Error saving private context: \(error.localizedDescription)")
-            finish(withError: error)
+            operationResult = Result { throw error }
+            return finish()
         }
     }
 }
@@ -63,11 +73,6 @@ extension CoreDataOperation {
 // MARK: - Private Methods
 
 extension CoreDataOperation {
-    
-    fileprivate func finish(withError error: Error?) {
-        self.error = error
-        finish()
-    }
     
     fileprivate func save(parentContext: NSManagedObjectContext?, completion: @escaping (Error?) -> ()) {
         guard let parentContext = parentContext, !isCancelled else {
