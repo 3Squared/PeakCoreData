1a5095d31ffc34904ef02891196585ac763a7c65
David Yates <david.yates@3squared.com>
2017-09-21 15:35:51 +0100

    Remove final

--- THRCoreData/Core/PersistentContainer.swift
+++ THRCoreData/Core/PersistentContainer.swift
@@ -12,7 +12,7 @@ import THRResult
 public typealias SetupCompletionType = (Result<PersistentStoreDescription>) -> ()
 
 /// A container that encapsulates the Core Data stack in your application.
-open final class PersistentContainer {
+open class PersistentContainer {
     
     internal enum ModelFileExtension: String {
         case bundle = "momd"
