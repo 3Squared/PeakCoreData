70e7b1d8e17150f5ba3b7036d4fa73a65ae0f4cc
David Yates <david.yates@3squared.com>
2018-03-22 11:40:05 +0000

    Remove duplicate set of automaticallyMergesChangesFromParent

--- Example/THRCoreDataExample/THRCoreDataExample/AppDelegate.swift
+++ Example/THRCoreDataExample/THRCoreDataExample/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "THRCoreDataExample")
-        container.viewContext.automaticallyMergesChangesFromParent = true
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
