1f4714503ec94f74dae32653e9e94247a6f27708
David Yates <david.yates@3squared.com>
2020-02-10 16:12:03 +0000

    Add more methods for caching objects

--- PeakCoreData/Core/Cache/ManagedObjectCache.swift
+++ PeakCoreData/Core/Cache/ManagedObjectCache.swift
@@ -22,11 +22,25 @@ public class ManagedObjectCache {
         return context.object(with: objectID) as? T
     }
     
+    public func object(with uniqueID: AnyHashable, entityName: String, in context: NSManagedObjectContext) -> NSManagedObject? {
+        let key = generateKey(forUniqueID: uniqueID, entityName: entityName)
+        guard let objectID = cache[key] else { return nil }
+        return context.object(with: objectID)
+    }
+    
+    public func register<T: CacheableObject>(_ objects: [T], in context: NSManagedObjectContext) {
+        do {
+            try context.obtainPermanentIDs(for: objects)
+            objects.forEach { register($0) }
+        } catch {
+            print("Error Obtaining Permanent ObjectIDs", error)
+        }
+    }
+    
     public func register<T: CacheableObject>(_ object: T, in context: NSManagedObjectContext) {
         do {
             try context.obtainPermanentIDs(for: [object])
-            let key = generateKey(forUniqueID: object.uniqueIDValue, entityName: T.entityName)
-            cache[key] = object.objectID
+            register(object)
         } catch {
             print("Error Obtaining Permanent ObjectID", error)
         }
@@ -40,4 +54,9 @@ public class ManagedObjectCache {
 extension ManagedObjectCache {
     
     private func generateKey(forUniqueID uniqueID: AnyHashable, entityName: String) -> String { entityName + String(uniqueID.hashValue) }
+    
+    private func register<T: CacheableObject>(_ object: T) {
+        let key = generateKey(forUniqueID: object.uniqueIDValue, entityName: T.entityName)
+        cache[key] = object.objectID
+    }
 }
