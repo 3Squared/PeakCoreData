3b4a839771719ba01a7e5ca9ae623015fb28ac9e
David Yates <d.j.yates@me.com>
2020-04-02 13:12:27 +0100

    Add Cache and ManagedObjectCache objects

---
+++ PeakCoreData/Core/Cache/Cache.swift
@@ -0,0 +1,81 @@
+//
+//  Cache.swift
+//  PeakCoreData-iOS
+//
+//  Created by David Yates on 02/04/2020.
+//  Copyright © 2020 3Squared Ltd. All rights reserved.
+//
+
+// Adapted from: https://www.swiftbysundell.com/articles/caching-in-swift/
+
+import Foundation
+
+/// Provides a wrapped around `NSCache` that doesn't require keys to subclass `NSObject`.
+public final class Cache<Key: Hashable, Value> {
+    
+    private let wrapped = NSCache<WrappedKey, Entry>()
+    
+    public init() {}
+    
+    public func insert(_ value: Value, forKey key: Key) {
+        let entry = Entry(value: value)
+        wrapped.setObject(entry, forKey: WrappedKey(key))
+    }
+
+    public func value(forKey key: Key) -> Value? {
+        let entry = wrapped.object(forKey: WrappedKey(key))
+        return entry?.value
+    }
+
+    public func removeValue(forKey key: Key) {
+        wrapped.removeObject(forKey: WrappedKey(key))
+    }
+    
+    public func removeAllObjects() {
+        wrapped.removeAllObjects()
+    }
+}
+
+extension Cache {
+    
+    public subscript(key: Key) -> Value? {
+        get { return value(forKey: key) }
+        set {
+            guard let value = newValue else {
+                // If nil was assigned using our subscript,
+                // then we remove any value for that key:
+                removeValue(forKey: key)
+                return
+            }
+            insert(value, forKey: key)
+        }
+    }
+}
+
+extension Cache {
+    
+    private final class WrappedKey: NSObject {
+        let key: Key
+
+        init(_ key: Key) {
+            self.key = key
+        }
+
+        override var hash: Int { key.hashValue }
+
+        override func isEqual(_ object: Any?) -> Bool {
+            guard let value = object as? WrappedKey else {
+                return false
+            }
+            return value.key == key
+        }
+    }
+    
+    private final class Entry {
+        let value: Value
+
+        init(value: Value) {
+            self.value = value
+        }
+    }
+}
---
+++ PeakCoreData/Core/Cache/ManagedObjectCache.swift
@@ -0,0 +1,59 @@
+//
+//  ManagedObjectCache.swift
+//  PeakCoreData-iOS
+//
+//  Created by David Yates on 02/04/2020.
+//  Copyright © 2020 3Squared Ltd. All rights reserved.
+//
+
+import CoreData
+
+public class ManagedObjectCache {
+    
+    public typealias CacheableObject = (ManagedObjectType & UniqueIdentifiable)
+    
+    private let cache = Cache<AnyHashable, NSManagedObjectID>()
+    
+    public init() {}
+        
+    public func object<T: CacheableObject>(withUniqueID uniqueID: AnyHashable, in context: NSManagedObjectContext) -> T? {
+        let key = generateKey(forUniqueID: uniqueID, entityName: T.entityName)
+        guard let objectID = cache[key] else { return nil }
+        return context.object(with: objectID) as? T
+    }
+    
+    public func object(with uniqueID: AnyHashable, entityName: String, in context: NSManagedObjectContext) -> NSManagedObject? {
+        let key = generateKey(forUniqueID: uniqueID, entityName: entityName)
+        guard let objectID = cache[key] else { return nil }
+        return context.object(with: objectID)
+    }
+    
+    public func register<T: CacheableObject>(_ object: T, in context: NSManagedObjectContext) {
+        register([object], in: context)
+    }
+    
+    public func register<T: CacheableObject>(_ objects: [T], in context: NSManagedObjectContext) {
+        do {
+            try context.obtainPermanentIDs(for: objects)
+            objects.forEach { register($0) }
+        } catch {
+            print("Error Obtaining Permanent ObjectID", error)
+        }
+    }
+    
+    public func removeAllObjects() {
+        cache.removeAllObjects()
+    }
+}
+
+extension ManagedObjectCache {
+    
+    private func register<T: CacheableObject>(_ object: T) {
+        let key = generateKey(forUniqueID: object.uniqueIDValue, entityName: T.entityName)
+        cache[key] = object.objectID
+    }
+    
+    private func generateKey(forUniqueID uniqueID: AnyHashable, entityName: String) -> String {
+        return entityName + String(uniqueID.hashValue)
+    }
+}
