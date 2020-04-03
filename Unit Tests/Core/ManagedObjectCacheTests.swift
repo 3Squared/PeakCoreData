1addd01e41b70622769f83987f60e48ba8ba8f0b
David Yates <d.j.yates@me.com>
2020-04-03 15:50:49 +0100

    Add further checks to cache

--- Unit Tests/Core/ManagedObjectCacheTests.swift
+++ Unit Tests/Core/ManagedObjectCacheTests.swift
@@ -63,6 +63,7 @@ class ManagedObjectCacheTests: CoreDataTests {
         XCTAssertEqual(evictedNumbers, [])
         
         cache.removeValue(forKey: "One")
+        XCTAssertEqual(cache.value(forKey: "One"), nil)
         XCTAssertEqual(evictedNumbers, [1])
     }
     
@@ -77,6 +78,7 @@ class ManagedObjectCacheTests: CoreDataTests {
         XCTAssertEqual(evictedNumbers, [])
         
         cache.clearCache()
+        XCTAssertEqual(cache.value(forKey: "One"), nil)
         XCTAssertEqual(evictedNumbers, [1])
     }
     
