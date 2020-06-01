8fd1f930c771ead4b14bdabed9e0edf81cc99f68
David Yates <david.yates@3squared.com>
2020-06-01 09:23:06 +0100

    Add batching to CoreDataBatchImportOperation

--- PeakCoreData/Core/Operations/CoreDataBatchImportOperation.swift
+++ PeakCoreData/Core/Operations/CoreDataBatchImportOperation.swift
@@ -1,5 +1,5 @@
 //
-//  ImportManyOperation.swift
+//  CoreDataBatchImportOperation.swift
 //  PeakCoreData
 //
 //  Created by Ben Walker on 15/12/2016.
@@ -9,38 +9,68 @@
 import CoreData
 import PeakOperation
 
-open class CoreDataBatchImportOperation<Intermediate>: CoreDataChangesetOperation, ConsumesResult where
-    Intermediate: ManagedObjectUpdatable & UniqueIdentifiable
-{
-    public var input: Result<[Intermediate], Error> = Result { throw ResultError.noResult }
+open class CoreDataBatchImportOperation<Intermediate>: CoreDataChangesetOperation, ConsumesResult where Intermediate: ManagedObjectUpdatable & UniqueIdentifiable {
     
     typealias ManagedObject = Intermediate.ManagedObject
     
+    public var input: Result<[Intermediate], Error> = Result { throw ResultError.noResult }
+    
+    private let batchSize: Int
+    private var batches: [[Intermediate]] = []
+    
+    public init(batchSize: Int = 50_000, cache: ManagedObjectCache? = nil, persistentContainer: NSPersistentContainer, mergePolicyType: NSMergePolicyType = .mergeByPropertyObjectTrumpMergePolicyType) {
+        self.batchSize = batchSize
+        super.init(persistentContainer: persistentContainer, cache: cache, mergePolicyType: mergePolicyType)
+    }
+
     open override func performWork(in context: NSManagedObjectContext) {
         do {
             let intermediates = try input.get()
             
-            let importProgress = Progress(totalUnitCount: Int64(intermediates.count) * 2)
+            let count = intermediates.count * ((Intermediate.hasProperties && Intermediate.hasRelationships) ? 2 : 1)
+            let importProgress = Progress(totalUnitCount: Int64(count))
             progress.addChild(importProgress, withPendingUnitCount: progress.totalUnitCount)
             
-            if Intermediate.hasProperties {
-                ManagedObject.insertOrUpdate(intermediates: intermediates, in: context, with: cache) { intermediate, managedObject in
-                    Intermediate.updateProperties?(intermediate, managedObject)
-                    importProgress.completedUnitCount += 1
-                }
-            }
-            
-            if Intermediate.hasRelationships {
-                ManagedObject.insertOrUpdate(intermediates: intermediates, in: context, with: cache) { intermediate, managedObject in
-                    Intermediate.updateRelationships?(intermediate, managedObject, context, cache)
-                    importProgress.completedUnitCount += 1
-                }
-            }
-            
-            saveAndFinish()
+            batches = intermediates.chunked(into: batchSize)
+            importNextBatch(in: context, importProgress: importProgress)
         } catch {
-            output = Result { throw error }
+            output = .failure(error)
             finish()
+        }
+    }
+    
+    func importNextBatch(in context: NSManagedObjectContext, importProgress: Progress) {
+        guard !isCancelled else { return finish() }
+        guard let intermediates = batches.first else {
+            return saveAndFinish()
+        }
+        
+        batches.removeFirst()
+        
+        if let updateProperties = Intermediate.updateProperties {
+            ManagedObject.insertOrUpdate(intermediates: intermediates, in: context, with: cache) { intermediate, managedObject in
+                updateProperties(intermediate, managedObject)
+                importProgress.completedUnitCount += 1
+            }
+        }
+        
+        if let updateRelationships = Intermediate.updateRelationships {
+            ManagedObject.insertOrUpdate(intermediates: intermediates, in: context, with: cache) { intermediate, managedObject in
+                updateRelationships(intermediate, managedObject, context, self.cache)
+                importProgress.completedUnitCount += 1
+            }
+        }
+        
+        saveOperationContext()
+        importNextBatch(in: context, importProgress: importProgress)
+    }
+}
+
+extension Array {
+    
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
         }
     }
 }
