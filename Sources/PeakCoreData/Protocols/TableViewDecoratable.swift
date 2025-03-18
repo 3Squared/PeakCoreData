8bb51cf8bcc1b5917f19cc86dcd0455205c41812
Zack Brown <zack.brown@velociti-solutions.com>
2025-03-18 08:50:00 +0000

    Add table view decoratable protocol

---
+++ Sources/PeakCoreData/Protocols/TableViewDecoratable.swift
@@ -0,0 +1,37 @@
+//
+//  TableViewDecoratable.swift
+//  PeakCoreData
+//
+//  Created by Zack Brown on 17/03/2025.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+public protocol TableViewDecoratable: AnyObject {
+    
+    // Optional
+    func identifier(forHeaderIn section: Int) -> String?
+    func identifier(forFooterIn section: Int) -> String?
+    func heightForHeader(in section: Int) -> CGFloat
+    func heightForFooter(in section: Int) -> CGFloat
+    func willDisplay(headerView: UIView, for section: Int)
+    func willDisplay(footerView: UIView, for section: Int)
+    func configure(header view: UIView, for section: Int)
+    func configure(footer view: UIView, for section: Int)
+}
+
+public extension TableViewDecoratable {
+    
+    func identifier(forHeaderIn section: Int) -> String? { nil }
+    func identifier(forFooterIn section: Int) -> String? { nil }
+    func heightForHeader(in section: Int) -> CGFloat { .leastNormalMagnitude}
+    func heightForFooter(in section: Int) -> CGFloat { .leastNormalMagnitude}
+    func willDisplay(headerView: UIView, for section: Int) { }
+    func willDisplay(footerView: UIView, for section: Int) { }
+    func configure(header view: UIView, for section: Int) { }
+    func configure(footer view: UIView, for section: Int) { }
+}
+
+#endif
